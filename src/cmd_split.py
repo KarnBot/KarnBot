2127a1a84247b3e7cea7c9750f103f744adf21dc
Glen Nelson <gralamin@gralamin.com>
2020-11-23 20:45:39 -0700

    Finish basic split func

--- src/cmd_split.py
+++ src/cmd_split.py
@@ -4,11 +4,21 @@ import random
 
 
 def split_group(people):
-    print("Starting split")
     num_tables = get_num_tables(len(people))
     tables = [[] for _ in range(num_tables)]
-    print(f"Determined I need {num_tables} tables")
+    random_order = randomize_people(people)
+    return assign_tables(random_order, tables)
+
+
+def randomize_people(people):
+    return random.shuffle(people)
 
 
 def get_num_tables(num_people):
     return max(int(math.ceil(num_people / 4.0)), 1)
+
+
+def assign_tables(people, tables):
+    for (i, person) in enumerate(people):
+        tables[i % len(tables)].append(person)
+    return tables
--- tests/test_cmd_split.py
+++ tests/test_cmd_split.py
@@ -1,13 +1,46 @@
 import unittest
+from unittest.mock import patch
 
 from karnbot import cmd_split
 
 
 class TestSplit(unittest.TestCase):
     def test_split_group(self):
-        cmd_split.split_group([])
-        self.assertTrue(True)
+        expected_order = ["Anson", "Eric", "Glen"]
+        with patch("random.shuffle", return_value=expected_order):
+            result = cmd_split.split_group(["Glen", "Eric", "Anson"])
+            self.assertListEqual(result[0], expected_order)
 
     def test_get_num_tables(self):
         self.assertEqual(cmd_split.get_num_tables(10), 3)
         self.assertEqual(cmd_split.get_num_tables(0), 1)
+
+    def test_randomize_people(self):
+        expected_order = ["Anson", "Eric", "Glen"]
+        with patch("random.shuffle", return_value=expected_order):
+            self.assertEqual(
+                cmd_split.randomize_people(["Glen", "Anson", "Eric"]),
+                expected_order,
+            )
+
+    def test_assign_tables__even(self):
+        people = ["Anson", "Eric", "Glen", "Ben", "Zac", "Chris"]
+        assigned = cmd_split.assign_tables(people, [[], []])
+        self.assertListEqual(assigned[0], ["Anson", "Glen", "Zac"])
+        self.assertListEqual(assigned[1], ["Eric", "Ben", "Chris"])
+
+        assigned = cmd_split.assign_tables(people, [[], [], []])
+        self.assertListEqual(assigned[0], ["Anson", "Ben"])
+        self.assertListEqual(assigned[1], ["Eric", "Zac"])
+        self.assertListEqual(assigned[2], ["Glen", "Chris"])
+
+    def test_assign_tables__odd(self):
+        people = ["Anson", "Eric", "Glen", "Ben", "Zac", "Chris", "Mike"]
+        assigned = cmd_split.assign_tables(people, [[], []])
+        self.assertListEqual(assigned[0], ["Anson", "Glen", "Zac", "Mike"])
+        self.assertListEqual(assigned[1], ["Eric", "Ben", "Chris"])
+
+        assigned = cmd_split.assign_tables(people, [[], [], []])
+        self.assertListEqual(assigned[0], ["Anson", "Ben", "Mike"])
+        self.assertListEqual(assigned[1], ["Eric", "Zac"])
+        self.assertListEqual(assigned[2], ["Glen", "Chris"])
