325d08963064be7c8b7b7f765719f2421fe334a1
Anson Li <hello@ansonli.ca>
2020-11-23 23:12:21 -0700

    Update bot.py

--- karnbot/bot.py
+++ karnbot/bot.py
@@ -27,7 +27,7 @@ async def on_ready():
 
     # Setup Config to get test channel details
     config = configparser.ConfigParser()
-    channel_id = config.get('Discord', 'TestChannel')
+    channel_id = config['Discord']['TestChannel']
 
     test_channel = bot.get_channel(channel_id)
     await bot.change_presence(
