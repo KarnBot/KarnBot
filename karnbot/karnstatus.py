06abfffc219f7cf434c1cfa261c88a1538c20f82
Glen Nelson <glen.nelson.1@gmail.com>
2022-10-20 21:55:04 -0600

    Fix build

--- karnbot/karnstatus.py
+++ karnbot/karnstatus.py
@@ -1,14 +1,17 @@
 import random
 
+
 def get_status():
-    return random.choice([
-        "Karn Liberated",
-        "Karn, Living Legacy",
-        "Karn's Bastion",
-        "Karn, Scion of Urza",
-        "Karn, Silver Golem",
-        "Karn's Sylex",
-        "Karn's Temporal Sundering",
-        "Karn's Touch",
-        "Karn, the Great Creator"
-    ])
+    return random.choice(
+        [
+            "Karn Liberated",
+            "Karn, Living Legacy",
+            "Karn's Bastion",
+            "Karn, Scion of Urza",
+            "Karn, Silver Golem",
+            "Karn's Sylex",
+            "Karn's Temporal Sundering",
+            "Karn's Touch",
+            "Karn, the Great Creator",
+        ]
+    )
